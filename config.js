5b0db133a1e49b695586f75cbc0ddc7b577e6377
FR33-KICK <80514181+FR33-KICK@users.noreply.github.com>
2021-03-12 10:55:16 +0100

    init
    init

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "prefix": ".",
+    "token_bot": "ODE5NjczNjkzODIyMzg2MjA2.YEqCnQ.49ro3V2-vlW5c15qwBnjyb0GTkg",
+    "supportServer": "supportServer" //if any
+     "colors": {
+      "error": "0xFF0000",
+      "success": "0x00FF46",
+      "warning": "0xFFD100"
+    },
+    "emotes": {
+    "music": "🎶 ",
+    "queue": "🎞️",
+    "error": "❌",
+    "success": "☑️",
+    "warning": "⚠️",
+    "repeat": "🔁",
+    "pause": "⏸️",
+    "resume": "⏯️",
+    "stop": "⏹️"
+    }
+}
